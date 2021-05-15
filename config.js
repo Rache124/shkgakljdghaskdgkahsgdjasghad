5fa9a51853aad9a589114d9392924d1bf56e0bdc
Rache124 <82086278+Rache124@users.noreply.github.com>
2021-05-15 12:22:55 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+    "ownerID": "611170675750862848", //kendi IDınızı yazınız
+    "admins": ["692731610214432819"],
+    "support": ["763077116945760296"],
+    "token": "NzcwMjgyMDIzNTUzODU5NjI0.X5bTCQ.sZoCS90ds_xXhMsU4hSO8wIXGV0", //botunuzun tokenini yazınız
+    "dashboard" : {
+      "oauthSecret": "WDuMLvloV99h-uH63fnpWQ1AvnDT7SkP", //botunuzun secretini yazınız
+      "callbackURL": `https://inquisitive-rose-zucchini.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "sessionSecret": "super-secret-session-thing", //kalsın
+      "domain": "https://inquisitive-rose-zucchini.glitch.me", //site URLnizi yazınız!
+          "port": process.env.PORT
+    }
+  };
+  
+  module.exports = config;
+
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+
+//GİZLYİE ALMASANIZ SUNUCUNUZ PATLIYABİLİR
